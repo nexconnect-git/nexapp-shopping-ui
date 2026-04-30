76ed78154c818b7f3c463a620c63b541037da075
nexconnect-git <haiia@nexconnect.com>
2026-05-01 00:50:54 +0530

    Remove legacy mobile wrappers

--- mobile-customer/android/app/src/main/java/com/nexconnect/customer/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.nexconnect.customer;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- mobile-delivery/android/app/src/main/java/com/nexconnect/delivery/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.nexconnect.delivery;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
